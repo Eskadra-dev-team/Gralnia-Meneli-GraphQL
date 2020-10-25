3ebf4994f2a37c73248c602b6fd45ad4a8c94288
Szymon Błaszczyk <szymon.a.blaszczyk@gmail.com>
2020-10-25 20:04:58 +0100

    Init Commit
    Tested Spring Initializer project seed

---
+++ src/test/java/pl/szymonblaszczyk/eskadra/graphql/GralniaMeneliGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.szymonblaszczyk.eskadra.graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GralniaMeneliGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
